0c15dae1a09447eff50bde684a2d1f23b1644386
krumilurfigur <gabrieloberg3@gmail.com>
2020-10-16 11:03:12 +0200

    fredag 16/10

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../models/db')
+const { pool, query } = require('../models/db')
 
 router.get('/', function (req, res, next) {
   const sql = 'SELECT * FROM story';
 
-  pool.query(sql, function (err, result, fields) {
+  query(sql, function (err, result, fields) {
     if (err) throw err;
     res.json({
       status: 200,
@@ -13,5 +13,7 @@ router.get('/', function (req, res, next) {
     });
   });
 });
+
+
 
 module.exports = router;
--- routes/links.js
+++ routes/links.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const pool = require('../models/db')
+const {pool, query} = require('../models/db')
 
 router.get('/', function (req, res, next) {
   const sql = 'SELECT * FROM links';
@@ -12,6 +12,32 @@ router.get('/', function (req, res, next) {
       result: result
     });
   });
+});
+
+router.get('/:id', async function (req, res, next) {
+const sql = 'SELECT * FROM story WHERE id = ?';
+
+try {
+  const story = await query(
+    'SELECT * FROM story WHERE id = ?',
+    req.params.id
+  );
+
+  const links = await query(
+    'SELECT * FROM links WHERE story_id = ?',
+    req.params.id
+  );
+
+  res.json({
+    status: 200,
+    story: story,
+    links: links
+  });
+
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
 });
 
 module.exports = router;
